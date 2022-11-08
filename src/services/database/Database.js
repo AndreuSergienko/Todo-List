db52159c62d1a6a41810fb2126138c4f484d75f9
Andrey <milanastrunevskaya@gmail.com>
2022-11-08 20:14:24 +0300

    init database services

---
+++ src/services/database/Database.js
@@ -0,0 +1,67 @@
+import { initializeApp } from 'firebase/app';
+import {
+	addDoc,
+	collection,
+	deleteDoc,
+	doc,
+	getDocs,
+	getFirestore,
+	updateDoc,
+} from 'firebase/firestore';
+import { API_KEY } from '../../constants/envValues';
+
+export class Database {
+	constructor() {
+		const firebaseConfig = {
+			apiKey: API_KEY,
+			authDomain: 'it-academy-todo-list.firebaseapp.com',
+			projectId: 'it-academy-todo-list',
+			storageBucket: 'it-academy-todo-list.appspot.com',
+			messagingSenderId: '943690172117',
+			appId: '1:943690172117:web:83e4bc6d8902ebb5f6c45c',
+			measurementId: 'G-WS01QDRLD0',
+		};
+
+		const app = initializeApp(firebaseConfig);
+		this._database = getFirestore(app);
+	}
+
+	create(collectionKey, body) {
+		const collectionRef = collection(this._database, collectionKey);
+
+		return addDoc(collectionRef, body);
+	}
+   
+	read(collectionKey) {
+		const collectionRef = collection(this._database, collectionKey);
+
+		return getDocs(collectionRef).then((documents) => {
+			return documents.docs.map((doc) => {
+				return {
+					...doc.data(),
+					id: doc.id,
+				};
+			});
+		});
+	}
+
+	update(collectionKey, id, body) {
+		const document = doc(this._database, collectionKey, id);
+
+		return updateDoc(document, body);
+	}
+
+	delete(collectionKey, id) {
+		const document = doc(this._database, collectionKey, id);
+
+		return deleteDoc(document);
+	}
+
+	static getInstance() {
+		if (!Database.instance) {
+			Database.instance = new Database();
+		}
+
+		return Database.instance;
+	}
+}
