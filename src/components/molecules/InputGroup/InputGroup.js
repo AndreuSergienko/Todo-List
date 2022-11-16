f2bd1234e4815c7b7a497278e0fd85ad25934709
Andrey <kolsergienko@gmail.com>
2022-11-16 20:17:59 +0300

    add sending-notification mechanism

--- src/components/molecules/InputGroup/InputGroup.js
+++ src/components/molecules/InputGroup/InputGroup.js
@@ -1,16 +1,23 @@
 import { Component } from '../../../core';
-import '../../atoms';
 import { todoList } from '../../../services';
+import '../../atoms';
 
 export class InputGroup extends Component {
 	constructor() {
 		super();
 		this.state = {
 			inputValue: '',
+			isSending: false
 		};
 	}
 
 	onSave() {
+		this.setState((state) => {
+			return {
+				...state,
+				isSending: true
+			}
+		})
 		todoList
 			.createTask({ title: this.state.inputValue, isCompleted: false })
 			.then(() => {
@@ -18,6 +25,7 @@ export class InputGroup extends Component {
 					return {
 						...state,
 						inputValue: '',
+						isSending: false
 					};
 				});
 			});
@@ -39,6 +47,29 @@ export class InputGroup extends Component {
 
 	render() {
 		return `
+		${this.state.isSending ?
+				`
+			<div
+		 	style="
+			position:fixed;
+			top:0;
+			left:0;
+			right:0;
+			bottom:0;
+			display:flex;
+			justify-content:center;
+			align-items:center;
+			z-index: 10;
+			background: rgba(0,0,0,0.5)
+			">
+				<div class="spinner-border" role="status">
+					<span class="visually-hidden">Loading...</span>
+				</div>
+		</div>
+			`
+				:
+				''
+			}
       <div class="input-group mb-3">
          <my-input value="${this.state.inputValue}" type="text" placeholder="Add a new task"></my-input>
          <my-button content="Save" classname="btn btn-outline-primary" eventtype="save-task"></my-button>
